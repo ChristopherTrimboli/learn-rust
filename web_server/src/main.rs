106b379955544010f203d34bee4db14fbe585cfe
Christopher Trimboli <cjft@trimboli.io>
2022-10-05 23:07:19 -0600

    basic webserver / client with Actix

---
+++ web_server/src/main.rs
@@ -0,0 +1,20 @@
+use actix_cors::Cors;
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let cors = Cors::permissive();
+        App::new().wrap(cors).service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await;
+
+    Ok(())
+}
