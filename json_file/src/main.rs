9a7371c9fb15bce9a8e39ca7076f988ff081174e
Christopher Trimboli <cjft@trimboli.io>
2022-10-08 23:23:21 -0600

    use serde, struct type check JSON, read file

--- json_file/src/main.rs
+++ json_file/src/main.rs
@@ -1,23 +1,29 @@
+use std::error::Error;
 use std::fs::File;
-use std::io;
-use std::io::prelude::*;
+use std::io::BufReader;
 use std::path::Path;
+use serde::Deserialize;
 
 const JSON_PATH: &str = "src/json_file.json";
 
-fn main() -> io::Result<()> {
+#[derive(Deserialize, Debug)]
+struct Json {
+    message: String,
+}
+
+
+fn main() -> Result<(), Box<dyn Error>> {
     let file_exists = Path::new(JSON_PATH)
         .try_exists()
         .expect("Can't check existence of file does_not_exist.txt");
     if file_exists {
-        let mut f = File::open(JSON_PATH)?;
-        let mut buffer = [0; 10];
-
-        let n = f.read(&mut buffer)?;
-
-        println!("The bytes: {:?}", &buffer[..n]);
+        let file = File::open(JSON_PATH)?;
+        let reader = BufReader::new(file);
+        let json: Json = serde_json::from_reader(reader)?;
+        println!("{:?}", json.message);
+        Ok(())
     } else {
         println!("File does not exist");
+        Err("File does not exist".into())
     }
-    Ok(())
 }
